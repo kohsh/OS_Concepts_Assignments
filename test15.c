9ddb6350aaa1882c198ce626a2859ec6cf8f2d37
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-10-06 07:53:43 -0700

    added source for test programs

---
+++ test15.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	sleep(15);
+	exit(0);
+}
---
+++ test5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	sleep(5);
+	exit(0);
+}
