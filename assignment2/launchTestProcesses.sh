e36458504e782a5c3bd61ce6433d22d669529857
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-10-17 23:28:25 -0700

    updated testing parameters

--- assignment2/launchTestProcesses.sh
+++ assignment2/launchTestProcesses.sh
@@ -1,22 +1,11 @@
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test5&
-./test15&
-./test15&
-./test15&
-./test15&
-./test15&
-./test15&
-./test15&
-./test15&
-./test15&
+#! /bin/bash
+#./test5&
+#./test5&
+#./test5&
+#./test5&
+#./test5&
+#./test15&
+#./test15&
+#./test15&
+#./test15&
 
